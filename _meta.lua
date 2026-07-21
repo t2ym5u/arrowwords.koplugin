e67325e090b4f26efeb6bfd156b9ecc8cd771b05
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-21 16:24:44 +0200

    chore: bump version to 1.2.3

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name        = "arrowwords",
     fullname    = _("Mots fléchés"),
     description = _("Arrow crossword puzzle"),
-    version     = "1.2.2",
+    version     = "1.2.3",
 }
